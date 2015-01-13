d9de03b0bfb640da57937d446bcb85f592fbecfb
Nishant <nishantpathak.cse@gmail.com>
2015-01-14 00:24:37 +0530

    initial commit

---
+++ Net Monitor/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.dropDowmInterface = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Seconds;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 36);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.CustomProperties = "IsXAxisQuantitative=True";
+            series1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            series1.Legend = "Legend1";
+            series1.LegendText = "Bytes In (#TOTAL{N0})";
+            series1.LegendToolTip = "#TOTAL{N0}";
+            series1.Name = "BytesIn";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+            series1.YValuesPerPoint = 4;
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.CustomProperties = "IsXAxisQuantitative=True";
+            series2.Legend = "Legend1";
+            series2.LegendText = "Bytes Out (#TOTAL{N0})";
+            series2.LegendToolTip = "#TOTAL{N0}";
+            series2.Name = "BytesOut";
+            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+            series2.YValuesPerPoint = 3;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(528, 258);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "Net Monitor";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // dropDowmInterface
+            // 
+            this.dropDowmInterface.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dropDowmInterface.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dropDowmInterface.FormattingEnabled = true;
+            this.dropDowmInterface.Location = new System.Drawing.Point(41, 319);
+            this.dropDowmInterface.Name = "dropDowmInterface";
+            this.dropDowmInterface.Size = new System.Drawing.Size(367, 21);
+            this.dropDowmInterface.TabIndex = 4;
+            this.dropDowmInterface.SelectedIndexChanged += new System.EventHandler(this.dropDown_IndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(540, 365);
+            this.Controls.Add(this.dropDowmInterface);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Net Monitor";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox dropDowmInterface;
+    }
+}
+
---
+++ Net Monitor/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private byte[] byteData = new byte[4096];
+        private BackgroundWorker backgroundWorker1;
+        private int inBytes;
+        private int outBytes;
+        private IPEndPoint ipEpOut;
+        private string srcIp = "";
+        private Socket socketIn;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string strIP = null;
+            inBytes = 0;
+
+            IPHostEntry HosyEntry = Dns.GetHostEntry((Dns.GetHostName()));
+            if (HosyEntry.AddressList.Length > 0)
+            {
+                foreach (IPAddress ip in HosyEntry.AddressList)
+                {
+                    strIP = ip.ToString();
+                    dropDowmInterface.Items.Add(strIP);
+                }
+
+            }
+        }
+
+        private void dropDown_IndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (socketIn != null)
+                {
+                    if (socketIn.IsBound)
+                    {
+                        socketIn.Close();
+
+                    }
+                }
+                srcIp = dropDowmInterface.Text;
+                IPEndPoint ipEp1 = new IPEndPoint(IPAddress.Parse(dropDowmInterface.Text), 0);
+                ipEpOut = new IPEndPoint(IPAddress.Parse(dropDowmInterface.Text), 0);
+                socketIn = new Socket(ipEp1.AddressFamily, SocketType.Raw, ProtocolType.IP);
+                socketIn.Bind(ipEp1);
+                byte[] byTrue = new byte[4] { 1, 0, 0, 0 };
+                byte[] byOut = new byte[4] { 1, 0, 0, 0 };
+                socketIn.BeginReceive(byteData, 0, byteData.Length, SocketFlags.None,
+                    new AsyncCallback(OnReceiveIn), null);
+
+                socketIn.IOControl(IOControlCode.ReceiveAll, byTrue, byOut);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+        }
+
+        private void OnReceiveIn(IAsyncResult ar)
+        {
+
+            int nReceived = 0;
+            IPHeader ipHeader = null;
+            try
+            {
+                nReceived = socketIn.EndReceive(ar);
+                ipHeader = new IPHeader(ref byteData, nReceived);
+            }
+            
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return;
+            }
+            System.Console.WriteLine("out" + ipHeader.SourceAddress.ToString());
+            if (ipHeader.SourceAddress.ToString() == srcIp) {
+                outBytes += nReceived;
+            }
+            else
+            {
+                inBytes += nReceived;
+            }
+            if (!backgroundWorker1.IsBusy)
+            {
+                this.backgroundWorker1.RunWorkerAsync();
+            }
+            socketIn.BeginReceive(byteData, 0, byteData.Length, SocketFlags.None,
+                new AsyncCallback(OnReceiveIn), null);
+        }
+        private void backgroundWorker1_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            if (chart1.Series.Count != 2) return;
+            System.Windows.Forms.DataVisualization.Charting.Series inSeries = chart1.Series[0];
+            System.Windows.Forms.DataVisualization.Charting.Series outSeries = chart1.Series[1];
+
+            inSeries.Points.AddXY(DateTime.Now, inBytes);
+            outSeries.Points.AddXY(DateTime.Now, outBytes);
+
+            inBytes = 0;
+            outBytes = 0;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+           // MessageBox.Show("Good bye" + e.ToString());
+            if (socketIn != null && socketIn.IsBound)
+            {
+                socketIn.Close();
+            }
+        }
+    }
+}
